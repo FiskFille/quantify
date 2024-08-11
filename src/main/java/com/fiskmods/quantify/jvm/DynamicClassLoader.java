00e07dc6a38a0b11a9007aafbab0d2417951ebfc
FiskFille <fisk.randenback@gmail.com>
2024-08-11 22:10:07 +0200

    Fixed custom ClassLoader setups breaking DynamicClassLoader

--- src/main/java/com/fiskmods/quantify/jvm/DynamicClassLoader.java
+++ src/main/java/com/fiskmods/quantify/jvm/DynamicClassLoader.java
@@ -1,7 +1,20 @@
 package com.fiskmods.quantify.jvm;
 
 public class DynamicClassLoader extends ClassLoader {
+    public DynamicClassLoader() {
+        /*
+         * Ensure that dynamically generated classes can always be cast to
+         * JvmRunnable, regardless of how it was loaded.
+         */
+        super(JvmRunnable.class.getClassLoader());
+    }
+
     public Class<?> defineClass(String name, byte[] b) throws ClassFormatError {
         return defineClass(name, b, 0, b.length);
+    }
+
+    @Override
+    public String toString() {
+        return "QTF-JVM-BRIDGE";
     }
 }
